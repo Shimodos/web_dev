06d2ee14746a5cf5400dd5cf28f0c5ac0217bc98
Alekssey Demchenko <ad@pesochin.net>
2023-01-12 00:09:01 +0200

    Practice_09 Site number input mask

--- Practice_09/src/js/script.js
+++ Practice_09/src/js/script.js
@@ -98,5 +98,8 @@ $(function() {
     validateForms('#consultation form');
     validateForms('#order form');
 
+    // $('input[name=phone]').mask("+7(999) 999-99-99");
+      $('input[name=phone]').mask("+7 (999) 999-99-99");
+
 });
 })(jQuery);
